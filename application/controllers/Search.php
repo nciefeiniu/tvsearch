23f2f434bea8a278112506b402c1dabdb742a451
nicefeiniu <872938058@qq.com>
2018-03-20 15:36:40 +0800

    add html

--- application/controllers/Search.php
+++ application/controllers/Search.php
@@ -3,20 +3,13 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 
 class Search extends CI_Controller {
 
-    public function __construct()
-    {
-        parent::__construct();
-        $this->load->model('search_model');
-        $this->load->helper('url_helper');
-    }
-
-    public function index()
-    {
-        $data['n'] = $this->search_model->get_tvid();
-    }
-
 	public function view($page = 'home')
 	{
+        if ( ! file_exists(APPATH.'views/pages/'.$page.'.php') )
+        {
+            show_404();
+        }
 
+        $this->load->view('pages'.$page);
 	}
 }
---
+++ application/views/pages/index.php
@@ -0,0 +1,41 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>犇云</title>
+
+    <link href="../css/main.css" rel="stylesheet">
+    <link href="../css/bootstrap.min.css" rel="stylesheet">
+    <script type="text/javascript" src="../js/bootstrap.min.js"></script>
+
+
+</head>
+<body background="https://cn.bing.com/az/hprichbg/rb/TulipsEquinox_ZH-CN11213785857_1920x1080.jpg">
+<div id="viewport">
+    <div class="content" id="main">
+        <div class="logo">
+            <div class="logo_">
+                <center>
+                <img id="logo_img" src="../images/logos.png"  >
+                </center>
+            </div>
+        </div>
+        <!--搜索框-->
+        <div class="search">
+            <form action="http://baidu.com" method="get">
+                <input type="text" class="form-control" placeholder="请输入剧名~  按回车键确认">
+            </form>
+        </div>
+        <!--&lt;!&ndash;提示框&ndash;&gt;-->
+        <!--<div class="alert alert-info sure" role="alert">-->
+            <!--<a href="#" class="alert-link">回车键确认</a>-->
+        <!--</div>-->
+    </div>
+
+    <!--copyright-->
+<div id="footer">
+    <em>&copy; 2018 <a href="https://github.com/nciefeiniu"> nicefeiniu</a></em>
+</div>
+</div>
+</body>
+</html>
